1ab6960e91e48b578d34c69483418e75a5afcd8d
AkshayaShanam <akshayashanam0123@gmail.com>
2025-09-06 10:55:03 +0530

    index.html into backend folder

--- student-form-backend/server.js
+++ student-form-backend/server.js
@@ -7,6 +7,13 @@ const studentRoutes = require("./routes/studentRoutes");
 const app = express();
 app.use(cors());
 app.use(express.json());
+const path = require("path");
+
+// Serve index.html at root
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
 app.use("/api/students", studentRoutes);
 
 const PORT = process.env.PORT || 5001;
