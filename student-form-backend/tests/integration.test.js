298a0014b2c2b84de362a8d21960a2a602275cb5
AkshayaShanam <akshayashanam0123@gmail.com>
2025-09-09 15:21:18 +0530

    integration.test.js

--- student-form-backend/tests/integration.test.js
+++ student-form-backend/tests/integration.test.js
@@ -1,5 +1,6 @@
 const request = require("supertest");
 const app = require("../server");
+const db = require("../config/db");
 
 describe("Student API Integration Tests", () => {
   it("should insert a new student", async () => {
